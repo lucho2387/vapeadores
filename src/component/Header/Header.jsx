be28076cf369b2e12b5d8a5bdcd3ff0448b9e6e5
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2021-11-10 20:21:23 -0300

    Update Header.jsx

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,27 +1,26 @@
 import './index.css';
+import Item from "../../components/Item/Item"
+import Item2 from "../../components/Item/Item2"
+import Enlace from "../../components/Enlace/Enlace"
+import Carrito from "../../components/CardWidget/CardWidget"
 
 const Header = () => {
     return (
             <header className = "header trans_300">
-                <a href="#" className = "menuHamburguesa" aria-hidden="true"><i class="fas fa-bars"></i></a>
-                <a href="#" className = "logo">Vaper<span>Shop</span></a>
+                <Enlace estilo="menuHamburguesa" icono="fas fa-bars"/>
+                <a href="#" className = "logo">Ecomm<span>erce</span></a>
                 <nav className="navbar">
                     <ul className="navbar_menu">
-                        <li><a href="#">Inicio</a></li>
-                        <li><a href="#">Tienda</a></li>
-                        <li><a href="#">Promociones</a></li>
-                        <li><a href="#">Nosotros</a></li>
-                        <li><a href="#">Contacto</a></li>
+                        <Item valor="Inicio"/>
+                        <Item valor="Tienda"/>
+                        <Item valor="Promociones"/>
+                        <Item valor="Nosotros"/>
+                        <Item valor="Contacto"/>
                     </ul>
                     <ul className="navbar_user">
-                        <li><a href="#"><i className="fa fa-search" arial-hidden="true"></i></a></li>
-                        <li><a href="#"><i className="fa fa-user" arial-hidden="true"></i></a></li>
-                        <li className="checkout">
-                        <a href="#" className = "enlace">
-                            <i className="fa fa-shopping-cart" arial-hidden="true"></i>
-                            <span id="checkout_items" className="checkout_items">2</span>
-                        </a>
-                        </li>
+                        <Item2 icono="fa fa-search" />
+                        <Item2 icono="fa fa-user" />
+                        <Carrito estiloLista = "checkout" estiloEnlace = "enlace" icono = "fa fa-shopping-cart" estiloSpan = "checkout_items" valor = "8" />
                     </ul>
                 </nav>
             </header>
