e97354bffd082c91408af7a9f499c77ccde114d9
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2021-11-18 15:34:13 -0300

    Update Modal.jsx

--- src/component/Modal/Modal.jsx
+++ src/component/Modal/Modal.jsx
@@ -3,13 +3,13 @@ import './modal.css'
 const Modal = ({ isOpen, numero, closeModal}) => {
     
     //Para que no se cierre al hacer click dentro del modal
-    const handleModalDialogClick = (e) => {
+    const onHandleModalDialogClick = (e) => {
         e.stopPropagation();
     }
     
     return (
         <div className={`modal ${numero > 0 && isOpen && 'modal-open'}`} onClick={closeModal}>
-            <div className="modal__dialog" onClick={handleModalDialogClick}>
+            <div className="modal__dialog" onClick={onHandleModalDialogClick}>
                 <h1 className="modaltitulo">El producto fue agregado correctamente</h1>
                 <button className="botonCerrar" onClick={closeModal}>
                     X
