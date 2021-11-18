72265c393a560a9dade9b8c00fa4129f3769ce61
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2021-11-18 15:34:41 -0300

    Create ModalMessageError.jsx

---
+++ src/component/Modal/ModalMessageError.jsx
@@ -0,0 +1,22 @@
+import './modal.css'
+
+const ModalMessageError = ({ isOpen, numero, closeModal}) => {
+    
+    //Para que no se cierre al hacer click dentro del modal
+    const onHandleModalDialogClick = (e) => {
+        e.stopPropagation();
+    }
+    
+    return (
+        <div className={`modal ${numero == 0 && isOpen && 'modal-open'}`} onClick={closeModal}>
+            <div className="modal__dialog" onClick={onHandleModalDialogClick}>
+                <h1 className="modaltitulo">La cantidad ingresada no es admitida</h1>
+                <button className="botonCerrar" onClick={closeModal}>
+                    X
+                </button>
+            </div>
+        </div>
+    )
+} 
+
+export default ModalMessageError
